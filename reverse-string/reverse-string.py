52239786804aa38e669bafdcd88e17e282d40024
Sean Se-Chang Park <psckyky@hotmail.com>
2021-07-12 19:48:19 +0900

    Time: 248 ms (9.43%), Space: 21.1 MB (80.98%) - LeetHub

--- reverse-string/reverse-string.py
+++ reverse-string/reverse-string.py
@@ -5,13 +5,5 @@ class Solution(object):
         :rtype: None Do not return anything, modify s in-place instead.
         """
         
-        i=0; j=len(s)-1
-        
-        while i<j:
-            if s[i] != s[j]:
-                t = s[j]
-                s[j] = s[i]
-                s[i] = t
-            i += 1; j-= 1
-            
+        s.reverse()
             
